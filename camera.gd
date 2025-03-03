1197065852ef3e438ae1e55c74b321d1cc6f542f
~ <37386853+jstri@users.noreply.github.com>
2025-03-03 12:40:08 +0000

    zoom with scroll wheel

--- camera.gd
+++ camera.gd
@@ -1,18 +1,7 @@
 extends Camera2D
 
-#var dragging = false
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("rclick"):
-		#dragging = true
-	#elif Input.is_action_just_released("rclick"):
-		#dragging = false
-	#
-	#if Input.is
-	#pass
-
-var dragging = false
+var dragging: bool = false
+var zoom_factor: Vector2 = Vector2(0.02, 0.02)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("rclick"):
@@ -22,3 +11,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if dragging and event is InputEventMouseMotion:
 		self.position -= event.relative / self.zoom
+
+	if event.is_action_pressed("zoom_in"):
+		self.zoom += zoom_factor
+	if event.is_action_pressed("zoom_out"):
+		self.zoom -= zoom_factor
